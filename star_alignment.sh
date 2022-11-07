ba2e096cacff999e7b594ce2eb92047c6a3a1024
Ann-Sophie Gironne <gironnea@wyvern.iric.ca>
2022-11-07 16:45:58 -0500

    working towards my general script

--- star_alignment.sh
+++ star_alignment.sh
@@ -1,4 +1,35 @@
-for file in ../scratch/data/sra_fastq/*.fastq
+module load star/2.7.1a
+
+set -eo pipefail
+
+genome=/soft/bioinfo/genomes/species/Mus_musculus.GRCm38/genome/star_index/GencodeM24.STAR.v2.7.1a.sjdbOverhang99
+
+for file in ./scratch/data/test
 do
-    wc -l $file
-done
+    echo "mapping $file"
+    R1=
+    time STAR --runMode alignReads \
+        --genomeDir $genome \
+        --readFilesIn $R1_files $R2_files \
+        --twopassMode Basic \
+        --outStd Log \
+        --outReadsUnmapped Fastx \
+        --outFilterType BySJout \
+        --outFilterMultimapNmax 20 \
+        --outFilterMismatchNmax 999 \
+        --outFilterMismatchNoverReadLmax 0.04 \
+        --alignIntronMin 20 \
+        --alignIntronMax 1000000 \
+        --alignMatesGapMax 1000000 \
+        --alignSJoverhangMin 8 \
+        --alignSJDBoverhangMin 1 \
+        --sjdbScore 1 \
+        --runThreadN 8 \
+        --limitBAMsortRAM 31532137230 \
+        --outSAMtype BAM Unsorted \
+        --outSAMunmapped Within \
+        --quantMode TranscriptomeSAM GeneCounts \
+        --outFileNamePrefix $file \
+        --outSAMattributes NH HI AS NM MD \
+
+mv $file* ./scratch/data/test
