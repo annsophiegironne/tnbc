032db52c74b8954aa074ff96737c36163d0f394c
Ann-Sophie Gironne <gironnea@wyvern.iric.ca>
2022-11-09 20:22:35 -0500

    made adjustments

--- star_alignment.sh
+++ star_alignment.sh
@@ -3,14 +3,21 @@ module load star/2.7.1a
 set -eo pipefail
 
 genome=/soft/bioinfo/genomes/species/Mus_musculus.GRCm38/genome/star_index/GencodeM24.STAR.v2.7.1a.sjdbOverhang99
+folder=/u/gironnea/tnbc/scratch/data/fastq
+output=/u/gironnea/tnbc/scratch/data/bam
 
-for file in ./scratch/data/test
+for file in $folder/*_1.fastq
 do
-    echo "mapping $file"
-    R1=
+    sample=`echo $file | grep -Eo 'SRR[0-9]{8}'`
+    R1_file=$folder/${sample}_1.fastq
+    R2_file=$folder/${sample}_2.fastq
+    echo "mapping $sample"
+    
+    mkdir $output/$sample
+    
     time STAR --runMode alignReads \
         --genomeDir $genome \
-        --readFilesIn $R1_files $R2_files \
+        --readFilesIn $R1_file $R2_file \
         --twopassMode Basic \
         --outStd Log \
         --outReadsUnmapped Fastx \
@@ -29,7 +36,13 @@ do
         --outSAMtype BAM Unsorted \
         --outSAMunmapped Within \
         --quantMode TranscriptomeSAM GeneCounts \
-        --outFileNamePrefix $file \
+        --outFileNamePrefix ${sample}_ \
         --outSAMattributes NH HI AS NM MD \
+        > $output/$sample/$sample.log
+    
+    echo "moving the files"
+    mv ${sample}_* $output/$sample
+done
 
-mv $file* ./scratch/data/test
+#si ca plante regarder si fichiers non vides deja presents pour pouvoir relancer direct :)
+#inclure fetch maybe yay
